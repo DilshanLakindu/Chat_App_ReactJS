bd72e52b532e86ae9a6ccbe773a54d217f25c465
Dilshan Lakindu <124870908+LakinduDasanayaka@users.noreply.github.com>
2023-02-16 17:05:40 +0530

    Registration page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Register from "./pages/Register";
+import "./style.scss";
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Add from "./../img/img.png";
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">ChatLINE</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="User name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
